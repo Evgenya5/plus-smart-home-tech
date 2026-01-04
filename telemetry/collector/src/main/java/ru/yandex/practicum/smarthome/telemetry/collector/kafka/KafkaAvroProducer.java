c4c01243456fb42e1725eb5e5bc08e141dba3469
Evgeniya <eva.sharapova1@mail.ru>
2026-01-04 19:48:38 +0300

    V2_2

--- telemetry/collector/src/main/java/ru/yandex/practicum/smarthome/telemetry/collector/kafka/KafkaAvroProducer.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/smarthome/telemetry/collector/kafka/KafkaAvroProducer.java
@@ -9,21 +9,31 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.kafka.serializer.GeneralAvroSerializer;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Properties;
 
 @Component
 public class KafkaAvroProducer {
 
+    @Value("${kafka.bootstrap.servers:localhost:9092}")
+    private String bootstrapServers;
+
+    @Value("${kafka.topics.sensors:telemetry.sensors.v1}")
+    private String sensorTopic;
+
+    @Value("${kafka.topics.hubs:telemetry.hubs.v1}")
+    private String hubTopic;
+
     public void sendSensorEvent(SensorEventAvro event) {
         Properties config = new Properties();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
 
         try (Producer<String, SensorEventAvro> producer = new KafkaProducer<>(config)) {
             ProducerRecord<String, SensorEventAvro> record =
-                    new ProducerRecord<>("telemetry.sensors.v1", event);
+                    new ProducerRecord<>(sensorTopic, event);
 
             producer.send(record);
         }
@@ -31,13 +41,13 @@ public class KafkaAvroProducer {
 
     public void sendHubEvent(HubEventAvro event) {
         Properties config = new Properties();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
 
         try (Producer<String, HubEventAvro> producer = new KafkaProducer<>(config)) {
             ProducerRecord<String, HubEventAvro> record =
-                    new ProducerRecord<>("telemetry.hubs.v1", event);
+                    new ProducerRecord<>(hubTopic, event);
 
             producer.send(record);
         }
