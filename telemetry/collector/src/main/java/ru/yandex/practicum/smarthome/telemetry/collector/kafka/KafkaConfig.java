6729cb37442e2803eafd31fcfc0e22b2cf83a0d3
Evgeniya <eva.sharapova1@mail.ru>
2026-01-11 23:57:10 +0300

    V1_10

--- telemetry/collector/src/main/java/ru/yandex/practicum/smarthome/telemetry/collector/kafka/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/smarthome/telemetry/collector/kafka/KafkaConfig.java
@@ -4,17 +4,11 @@ import lombok.Getter;
 import lombok.Setter;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.Duration;
 import java.util.Properties;
 
 @Getter
@@ -27,10 +21,6 @@ public class KafkaConfig {
     private String bootstrapServers;
     private String keySerializer;
     private String valueSerializer;
-
-    /*// Конфиги
-    @Autowired
-    private CollectorProducerConfig producerConfig;*/
 
     @Bean
     public KafkaProducer<String, SpecificRecordBase> initProducer(KafkaProperties properties) {
