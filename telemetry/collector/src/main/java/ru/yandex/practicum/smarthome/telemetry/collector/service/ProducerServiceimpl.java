56241f925b98dc8171ba25e69cf9f0b9c587c4ef
Evgeniya <eva.sharapova1@mail.ru>
2026-01-11 23:42:06 +0300

    V1_9

--- telemetry/collector/src/main/java/ru/yandex/practicum/smarthome/telemetry/collector/service/ProducerServiceimpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/smarthome/telemetry/collector/service/ProducerServiceimpl.java
@@ -1,9 +1,5 @@
 package ru.yandex.practicum.smarthome.telemetry.collector.service;
 
-import org.apache.avro.specific.SpecificRecordBase;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
@@ -25,17 +21,16 @@ public class ProducerServiceimpl implements ProducerService {
     @Value("${collector.kafka.topics.hubs}")
     private String hubsEventsTopic;
 
-    private final Producer<String, SpecificRecordBase> producer;
+    private final KafkaClient kafkaClient;
     private final Map<SensorEventProto.PayloadCase, SensorEventMapper> sensorEventMappers;
     private final Map<HubEventProto.PayloadCase, HubEventMapper> hubEventMappers;
 
-    @Autowired
     public ProducerServiceimpl(
             KafkaClient kafkaClient,
             List<SensorEventMapper> sensorEventMappers,
             List<HubEventMapper> hubEventMappers
     ) {
-        this.producer = kafkaClient.getProducer();
+        this.kafkaClient = kafkaClient;
         this.sensorEventMappers = sensorEventMappers.stream()
                 .collect(Collectors.toMap(SensorEventMapper::getSensorEventType, Function.identity()));
         this.hubEventMappers = hubEventMappers.stream()
@@ -45,11 +40,11 @@ public class ProducerServiceimpl implements ProducerService {
     @Override
     public void processSensorEvent(SensorEventProto sensorEventProto) {
         if (sensorEventMappers.containsKey(sensorEventProto.getPayloadCase())) {
-            producer.send(new ProducerRecord<>(
+            kafkaClient.send(
                     sensorsEventsTopic,
                     sensorEventProto.getHubId(),
                     sensorEventMappers.get(sensorEventProto.getPayloadCase()).mapToAvro(sensorEventProto)
-            ));
+            );
         } else {
             throw new IllegalArgumentException("Нет подходящего маппера");
         }
@@ -58,11 +53,11 @@ public class ProducerServiceimpl implements ProducerService {
     @Override
     public void processHubEvent(HubEventProto hubEventProto) {
         if (hubEventMappers.containsKey(hubEventProto.getPayloadCase())) {
-            producer.send(new ProducerRecord<>(
+            kafkaClient.send(
                     hubsEventsTopic,
                     hubEventProto.getHubId(),
                     hubEventMappers.get(hubEventProto.getPayloadCase()).mapToAvro(hubEventProto)
-            ));
+            );
         } else {
             throw new IllegalArgumentException("Нет подходящего маппера");
         }
