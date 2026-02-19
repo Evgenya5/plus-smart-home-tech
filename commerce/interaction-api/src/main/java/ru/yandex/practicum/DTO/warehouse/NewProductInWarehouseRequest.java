49437279c0506c1d21138ac97fa6ec9bc711b819
Evgeniya <eva.sharapova1@mail.ru>
2026-02-19 22:06:05 +0300

    change warehouse

--- commerce/interaction-api/src/main/java/ru/yandex/practicum/DTO/warehouse/NewProductInWarehouseRequest.java
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/DTO/warehouse/NewProductInWarehouseRequest.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.DTO.warehouse;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -14,21 +13,15 @@ import java.util.UUID;
 @AllArgsConstructor
 public class NewProductInWarehouseRequest {
 
-    @NotNull(message = "ID продукта != null.")
+    @NotNull
     private UUID productId;
 
-    @NotNull(message = "Описание товара != null.")
-    @Valid
+    private Boolean fragile;
+
+    @NotNull
     private DimensionDto dimension;
 
-    @Min(value = 0, message = "Количество товара не может быть отрицательным.")
-    @Builder.Default
-    private Long quantity = 0L;
-
-    @Min(value = 1, message = "Вес товара не может быть меньше 1.")
-    @Builder.Default
-    private Double weight = 1.0;
-
-    @Builder.Default
-    private Boolean fragile = true;
+    @NotNull
+    @Min(value = 1)
+    private Double weight;
 }
