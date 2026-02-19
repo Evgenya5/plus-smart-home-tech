59b471d43092f190f031037dcb10fdf86d1491bc
Evgeniya <eva.sharapova1@mail.ru>
2026-02-20 00:32:23 +0300

    change

---
+++ commerce/warehouse/src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS warehouse_products (
+    product_id UUID PRIMARY KEY,
+    quantity BIGINT NOT NULL DEFAULT 0,
+    fragile BOOLEAN DEFAULT FALSE,
+    width DOUBLE PRECISION,
+    height DOUBLE PRECISION,
+    depth DOUBLE PRECISION,
+    weight DOUBLE PRECISION
+);
